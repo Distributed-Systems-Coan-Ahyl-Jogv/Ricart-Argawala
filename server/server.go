d6df91368a5311e4602bfc9e5ff0f26d6f015fa3
Andreas <ahyl@itu.dk>
2025-11-06 14:04:03 +0100

    added to server, not working

--- server/server.go
+++ server/server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	proto "ARGAWALAServer/grpc"
+	"bufio"
 	"fmt"
 	"net"
 	"os"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 type ArgawalaServer struct {
@@ -25,5 +27,19 @@ func main() {
 	err = server.Serve(listener)
 	if err != nil {
 		panic(err)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "" || len(text) > 128 {
+			continue
+		}
+		conn, err := grpc.Dial(text, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			fmt.Println("Failed to connect:", err)
+			continue
+		}
+		defer conn.Close()
 	}
 }
